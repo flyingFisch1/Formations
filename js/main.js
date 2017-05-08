e302846c711b6f1b98347f818485259f4f6cdad8
flyingFisch <vinitamutha19@gmail.com>
2017-05-08 11:42:53 +0530

    First commit, added all files to git

---
+++ js/main.js
@@ -0,0 +1,126 @@
+$(document).ready(function() {
+    $('.particles-js-canvas-el').css('position', 'absolute');
+    $(".button-collapse").sideNav();
+
+    $(window).scroll(function() {
+        parallax();
+    });
+});
+
+function parallax() {
+    var Yvalue = $(window).scrollTop();
+    $('.logo').css('top', (-(Yvalue * 0.5)) + 'px');
+    $('#particles-js').css('top', (-(Yvalue * 1.4)) + 'px');
+}
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 75,
+            "density": {
+                "enable": true,
+                "value_area": 631.3280775270874
+            }
+        },
+        "color": {
+            "value": "#1d10f0"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 3
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#000000",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6.667611244926368,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "grab"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
